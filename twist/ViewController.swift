b92b66cd9ae5e3a3c3a9c22b9109c53301ab3401
Dmitry <d.myachn@gmail.com>
2024-09-11 10:53:58 +0300

    Initial Commit

---
+++ twist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twist
+//
+//  Created by Dmitriy Myachin on 11.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
